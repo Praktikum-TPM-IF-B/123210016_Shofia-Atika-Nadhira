8c25516b566d26a63c15f14651d2a3fe0f639c97
snadhiraa2@gm <shofia1912@gmail.com>
2024-06-15 13:38:04 +0700

    project-akhir

--- kuis_elearning/android/app/src/main/kotlin/com/example/kuis_elearning/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kuis_elearning
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- post_app/android/app/src/main/kotlin/com/example/post_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.post_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
